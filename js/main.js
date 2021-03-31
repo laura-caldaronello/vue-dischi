c7a341ab8c6f58574ef7c1e6b8f0afe51e852053
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-31 12:29:51 +0200

    Bonus ordine per anno di uscita

--- js/main.js
+++ js/main.js
@@ -26,6 +26,21 @@ var app = new Vue({
             });
             console.log(this.genres);
 
+            // ordine per anno: si potrebbe utilizzare il sort ma voglio farlo "artigianalmente"
+            for (let k = 0; k < this.albums.length; k++) {
+                var i = 0;
+                while (i < this.albums.length - 1) {
+                    if (parseInt(this.albums[i].year) > parseInt(this.albums[i + 1].year)) {
+                        // scambio di posizione in modo che il più grande venga dopo
+                        let bigger = this.albums[i];
+                        let smaller = this.albums[i + 1];
+                        this.albums[i] = smaller;
+                        this.albums[i + 1] = bigger;
+                    }
+                    // successivamente aumento l'indice, per continuare a confrontare il bigger finchè il più grande di tutti non è in fondo; tutta questa operazione va fatta tante volte quanti sono gli elementi dell'array albums, così ognuno degli elementi ha l'opportunità di essere messo per ultimo se è il più grande (per questo il for)
+                    i++;
+                };
+            };
         });
 
     }
